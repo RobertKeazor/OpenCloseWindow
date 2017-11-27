e2afda780a17b47bd699736d9474adb159e42cf5
Robert Keazor <robertkeazor@RobertsMBP86786.fios-router.home>
2017-11-26 23:41:16 -0500

    Turn button into ToggleButton and use selector

--- app/src/main/java/smartdrive/neube/example/com/openclosewindow/BackViewModel.kt
+++ app/src/main/java/smartdrive/neube/example/com/openclosewindow/BackViewModel.kt
@@ -1,10 +1,4 @@
 package smartdrive.neube.example.com.openclosewindow
-
 import android.arch.lifecycle.ViewModel
-import android.view.View
 
-class BackViewodel : ViewModel() {
-    fun updateDrawableResource(view: View){
-        view.setBackgroundResource(Fireback().imageId())
-    }
-}
+class BackViewodel : ViewModel()
